1e9f645ba90aa365759231ff1a3cfa901e6e6511
Miriam_Sallnäs <miriamfly54@hotmail.com>
2018-08-22 20:25:23 +0200

    Refactored

--- src/js./person.js
+++ src/js./person.js
@@ -4,7 +4,7 @@ function Person(attr) {
     this.height = attr.height;
 };
 
-Person.prototype.calculate_bmi = function () {
+Person.prototype.calculate_metric_bmi = function () {
     calculator = new BMICalculator();
     calculator.metric_bmi(this)         // this is == to user person
 };                                      // CORB happens, security block
